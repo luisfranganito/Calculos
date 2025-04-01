dd6c8929972b11789a6cf3a5f1cd3ae1c93df476
Luis Franganito <luis_franganito@hotmail.com>
2025-04-01 22:34:27 +0100

    Adicionar método Subtrair()

--- Aritemetica.cs
+++ Aritemetica.cs
@@ -15,5 +15,21 @@ namespace Calculos
         {
             return x + y;
         }
+
+        public int Subtrair(int x, int y)
+        {
+            return x - y;
+        }
+
+
+
+
+
+
+
     }
+
+
+
+
 }
