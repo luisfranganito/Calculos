8a7009b528541ad577c6dfe81dfb8eced808c598
Luis Franganito <luis_franganito@hotmail.com>
2025-04-01 22:43:02 +0100

    Criar a classe Conversoes e mover método

--- Aritemetica.cs
+++ Aritemetica.cs
@@ -38,7 +38,19 @@ namespace Calculos
             CelsiusFahrenheit,
             FahrenheitCelsius
         }
+        public double ConverterTemperatura(ConversaoTemperatura conversao, double temperatura)
+        {
+            if (conversao == ConversaoTemperatura.CelsiusFahrenheit)
+            {
+                return (temperatura * 1.8000 + 32);
+            }
+            else if (conversao == ConversaoTemperatura.FahrenheitCelsius)
+            {
+                return ((temperatura - 32) / 1.8000);
+            }
 
+            return -1;
+        }
 
 
 
---
+++ Conversoes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculos
+{
+    /// <summary>
+    /// Conversão de temperaturas.
+    /// </summary>
+    /// <param name="conversao">A conversão a efetuar.</param>
+    /// <param name="temperatura">A temperatua a converter.</param>
+    /// <returns>Retorna o resultado da conversão da temperatura.</returns>
+    internal class Conversoes
+    {
+    }
+}
